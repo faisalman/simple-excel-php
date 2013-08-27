f573377e99b157cdd828c9c10b6be49ebe4cbb5b
Faisal Salman <fyzlman@gmail.com>
2013-08-27 14:19:18 +0700

    Initial work on XLSX parser

--- src/SimpleExcel/Parser/XLSXParser.php
+++ src/SimpleExcel/Parser/XLSXParser.php
@@ -2,6 +2,12 @@
  
 namespace SimpleExcel\Parser;
 
+use SimpleExcel\Enums\Datatype;
+use SimpleExcel\Enums\SimpleExcelException;
+use SimpleExcel\Spreadsheet\Cell;
+use SimpleExcel\Spreadsheet\Workbook;
+use SimpleExcel\Spreadsheet\Worksheet;
+
 /**
  * SimpleExcel class for parsing Microsoft Excel XLSX Spreadsheet
  *  
@@ -19,10 +25,29 @@ class XLSXParser extends BaseParser
     protected $file_extension = 'xlsx';
 
     /**
+    * Load the file to be parsed
+    * 
+    * @param    string  $file_path  Path to file
+    * @param    array   $options    Options
+    * @throws   Exception           If file being loaded doesn't exist
+    * @throws   Exception           If file extension doesn't match
+    * @throws   Exception           If error reading the file
+    */
+    public function loadFile ($file_path, $options = NULL) {
+        if ($this->checkFile($file_path)) {
+            // TODO: extract zip & map string to worksheet
+        }
+    }
+
+    /**
     * Load the string to be parsed
     * 
     * @param    string  $str        String with XLSX format
-	* @param    array   $options    Options
+    * @param    array   $options    Options
     */
-    public function loadString($str, $options) { }
+    public function loadString($str, $options) {
+        $this->workbook = new Workbook();
+        $xml = simplexml_load_string($str);
+        // TODO: implement xlsx parser
+    }
 }
